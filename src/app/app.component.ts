9639ad345d8cfbe6ee2aefabda8b17c346ba3938
Jose Chajtur <chajtur@gmail.com>
2022-07-31 21:55:59 -0600

    Haciendo primer push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Frontend';
+  title = 'Pariendo Angular con Bootstrap';
 }
